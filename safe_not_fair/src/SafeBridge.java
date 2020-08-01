1ee8401e71461a9fe65c167183f771b7d65fb2c0
rafchriz <32577771+rafchriz@users.noreply.github.com>
2020-08-01 04:20:19 +0300

    Update SafeBridge.java

--- safe_not_fair/src/SafeBridge.java
+++ safe_not_fair/src/SafeBridge.java
@@ -1,16 +1,13 @@
 import java.util.Date;
 
-//gia ti sigkekrimeni class stirihtika ston kodika pou mas dothike kata tin anathesi tis ergasias
 public class SafeBridge extends Bridge{
    
-	//o arithmos ton ditikon kai anatolikon autokiniton poy vriskonte mesa sti gefira
+	//number of west and east cars on bridge
 	private int nwest=0;
     private int neast=0;
 
     public synchronized void upWestExited(WestCar f) throws InterruptedException {
     	
-    	//mionete o arithmos ton autokiniton pou vriskonte sti gefira ke otan den ehi alla
-    	//ksipnai ta threads oste na sinehisoun apo eki pou ixane mini
     	--nwest;
     	if (nwest==0)
     		notifyAll();
@@ -20,8 +17,6 @@ public class SafeBridge extends Bridge{
 
     public synchronized void upEastExited(EastCar f) throws InterruptedException {
     	
-    	//mionete o arithmos ton autokiniton pou vriskonte sti gefira ke otan den ehi alla
-    	//ksipnai ta threads oste na sinehisoun apo eki pou ixane mini
     	--neast;
     	if (neast==0)
     		notifyAll();
@@ -32,8 +27,7 @@ public class SafeBridge extends Bridge{
     
     public synchronized void westCross(WestCar f) throws InterruptedException {
 
-    	//ginete eleghos ean vriskonte pano sti gefira ite ditika ite anatolika autokinita
-    	//oste an vriskete kapio na min mpi allo mesa ke na pernai ena kathe fora
+    	//check for cars on bridge to avoid the entrance of others 
     	while (neast>0 || nwest>0) wait();
     	++nwest;
         System.out.println(f.getID()+": Crossing bridge ..." + new Date());
@@ -51,8 +45,7 @@ public class SafeBridge extends Bridge{
     
     public synchronized void eastCross(EastCar f) throws InterruptedException {
 
-    	//ginete eleghos ean vriskonte pano sti gefira ite ditika ite anatolika autokinita
-    	//oste an vriskete kapio na min mpi allo mesa ke na pernai ena kathe fora
+    	//check for cars on bridge to avoid the entrance of others 
     	while (nwest>0 || neast>0) wait();
     	++neast;
     	System.out.println(f.getID()+": Crossing bridge ..." + new Date());
