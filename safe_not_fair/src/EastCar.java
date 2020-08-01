9cb4232b515494305142ae831629307248d6ab18
rafchriz <32577771+rafchriz@users.noreply.github.com>
2020-08-01 04:15:30 +0300

    Update EastCar.java

--- safe_not_fair/src/EastCar.java
+++ safe_not_fair/src/EastCar.java
@@ -4,8 +4,7 @@ public class EastCar implements Runnable{
     
     private String id;          
     private Bridge bridge;      
-    private boolean finished=false; //metavliti pou diloni ehi oloklirothi to treksimo tou ekastote thread
-
+    private boolean finished=false; //has thread finished it's task?
     public EastCar(String id, Bridge bridge) {
         this.id=id;
         this.bridge = bridge;
@@ -21,7 +20,7 @@ public class EastCar implements Runnable{
       try {
         while (!finished)
         {
-        	//to thread ehi ksekinisi alla den ehi mpi akomi sti gefira to autokinito
+        	//thread has started but car is not on bridge yet
     		System.out.println(this.getID()+": Waiting for bridge. Going towards West " + new Date());
     		doProcessing();
         }
@@ -29,9 +28,9 @@ public class EastCar implements Runnable{
     }
 
     private void doProcessing () throws InterruptedException {
-    	//to autokinito mpeni sti gefira
+    	//car enters bridge
     	bridge.eastCross(this);
-    	//to autokinito vgeni apo ti gefira
+    	//car exits bridge
     	bridge.upEastExited(this);
     	finished=true;
     }
