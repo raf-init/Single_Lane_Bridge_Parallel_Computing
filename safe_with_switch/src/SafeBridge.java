4fc74387081afcb850c5c975a04d3626c8e724f8
rafchriz <32577771+rafchriz@users.noreply.github.com>
2020-08-01 04:28:19 +0300

    Update SafeBridge.java

--- safe_with_switch/src/SafeBridge.java
+++ safe_with_switch/src/SafeBridge.java
@@ -1,17 +1,14 @@
 import java.util.Date;
 
-//gia ti sigkekrimeni class stirihtika ston kodika pou mas dothike kata tin anathesi tis ergasias
 public class SafeBridge extends Bridge{
 	
-	//o arithmos ton ditikon kai anatolikon autokiniton poy vriskonte mesa sti gefira
+	//number of west and east cars on bridge
     private int nwest=0;
     private int neast=0;
-    private boolean westturn = true; //diloni an ine i sira ton ditikon autokiniton
+    private boolean westturn = true; //whose turn is it?
 
     public synchronized void upWestExited(WestCar f) throws InterruptedException {
     	
-    	//mionete o arithmos ton autokiniton pou vriskonte sti gefira ke otan den ehi alla
-    	//ksipnai ta threads oste na sinehisoun apo eki pou ixane mini
     	--nwest;
     	westturn = false;
     	if (nwest==0)
@@ -20,9 +17,7 @@ public class SafeBridge extends Bridge{
     }
 
     public synchronized void upEastExited(EastCar f) throws InterruptedException {
-    	
-    	//mionete o arithmos ton autokiniton pou vriskonte sti gefira ke otan den ehi alla
-    	//ksipnai ta threads oste na sinehisoun apo eki pou ixane mini
+   
     	--neast;
     	westturn = true;
     	if (neast==0)
@@ -33,9 +28,8 @@ public class SafeBridge extends Bridge{
     
     public synchronized void westCross(WestCar f) throws InterruptedException {
 
-    	//ginete eleghos ean vriskonte pano sti gefira ite ditika ite anatolika autokinita
-    	//oste an vriskete kapio na min mpi allo mesa ke na pernai ena kathe fora
-    	//episis ginete eleghos an ine i sira ton ditikon autokiniton 
+    	//check if there are cars on bridge to prevent others from entering
+	//check whose turn is it
     	while (neast>0 || nwest>0 || !westturn) wait();
     	++nwest;
         System.out.println(f.getID()+": Crossing bridge ..." + new Date());
@@ -52,9 +46,8 @@ public class SafeBridge extends Bridge{
     
     public synchronized void eastCross(EastCar f) throws InterruptedException {
 
-    	//ginete eleghos ean vriskonte pano sti gefira ite ditika ite anatolika autokinita
-    	//oste an vriskete kapio na min mpi allo mesa ke na pernai ena kathe fora
-    	//episis ginete eleghos an ine i sira ton ditikon autokiniton 
+    	//check if there are cars on bridge to prevent others from entering
+	//check whose turn is it
     	while (nwest>0 || neast>0 || westturn) wait();
     	++neast;
     	System.out.println(f.getID()+": Crossing bridge ..." + new Date());
